1c3f1e2867fb6c873915a6bdff3a371dec3c7186
zfz9526 <zfz9526@163.com>
2023-04-03 00:59:07 +0800

    完成初始化配置，正常启动Application

---
+++ reade-forum-start/src/main/java/cn/bugstack/reade/forum/start/StartApplication.java
@@ -0,0 +1,20 @@
+package cn.bugstack.reade.forum.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: reade-forum
+ * @BelongsPackage: org.example
+ * @Author: zhd
+ * @CreateTime: 2023-04-02  22:59
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class,args);
+    }
+}
